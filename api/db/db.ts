067d690cf5b9d3a95b2c55d8c1e8a3c732fd25ba
tinow04 <tino.wolter2@gmail.com>
2025-07-06 14:15:50 +0200

    change db config

--- api/db/db.ts
+++ api/db/db.ts
@@ -10,5 +10,8 @@ export const db = new Pool({
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
-    port: Number(process.env.DB_PORT)
+    port: Number(process.env.DB_PORT),
+    ssl: {
+        rejectUnauthorized: false, // Disable strict SSL validation.
+    },
 });
