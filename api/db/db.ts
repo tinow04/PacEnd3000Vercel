fd243a383bc16b9735769dd8f2e34a03312d1dfe
tinow04 <tino.wolter2@gmail.com>
2025-07-06 14:13:55 +0200

    delete ssl

--- api/db/db.ts
+++ api/db/db.ts
@@ -10,6 +10,5 @@ export const db = new Pool({
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
-    port: Number(process.env.DB_PORT),
-    ssl: false
+    port: Number(process.env.DB_PORT)
 });
